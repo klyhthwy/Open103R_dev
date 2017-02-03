08ff60d89862c69a2d5e41e5d34a6fdd6c35f850
Kelly Hathaway <klyhthwy@gmail.com>
2017-02-03 00:11:37 -0700

    Did a basic LED counting example after implementing some GPIO functions.

--- Source/main.c
+++ Source/main.c
@@ -12,9 +12,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include "stm32f1xx.h"
+#include "kly_gpio.h"
 #include "kly_error.h"
 
+
+#define LED_PORT    2
+#define LED_POS     9
+#define LED_MASK    (0xF << LED_POS)
 
 
 void HardFault_Handler(void)
@@ -49,9 +53,19 @@ static void init_clock(void)
 
 int main(void)
 {
+    int a;
+
     init_clock();
 
+    kly_gpio_port_config(LED_PORT, LED_MASK,
+        KLY_GPIO_CONFIG_OUTPUT_PUSH_PULL, KLY_GPIO_PULL_NONE);
+
+    a = 0;
     for(;;)
     {
+        kly_gpio_port_write(LED_PORT, LED_MASK, a << LED_POS);
+        a++;
+        a %= 0x10;
+        for(int i = 0; i < 0x40000; i++);
     }
 }
